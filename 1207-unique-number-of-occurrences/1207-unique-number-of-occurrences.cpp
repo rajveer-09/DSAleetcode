21b96625fc39b8de88ce6223d8604658fdb50de6
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-10 02:13:32 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (79.46%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int cnt = 1;
+        vector<int> ans;
+        int j = 1;
+        sort(arr.begin(), arr.end());
+        for(int i = 0; i < arr.size()-1; i++){
+            if(arr[i] == arr[j]){
+                j++;
+                cnt++;
+                continue;
+            }
+            // ans.push_back(cnt);
+            else if(arr[i] != arr[j]){
+                ans.push_back(cnt);
+                j += 1;
+                cnt = 1;
+                continue;
+            }
+            ans.push_back(cnt);
+            cnt = 1;
+        }
+        ans.push_back(cnt);
+        sort(ans.begin(), ans.end());
+        for(int i = 0; i < ans.size()-1; i++){
+            if(ans[i] == ans[i+1]){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
