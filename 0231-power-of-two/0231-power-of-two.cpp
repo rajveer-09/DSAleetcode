4190403cfe1b7b4218a388d3057b225fd4418c8a
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-10 01:18:38 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (93.11%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        
-        if(n==0) return false;
-        while(n%2==0) n/=2;
-        return n==1;
+        if (n <= 0) {
+        return false;
+    }
+
+    // A number is a power of two if and only if it has exactly one set bit (binary).
+    // So, we use the bitwise AND operation to check for this condition.
+    return (n & (n - 1)) == 0;
         
     }
 };
