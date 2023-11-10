48bba8a576c3fe6ed1a46cf8f327cf2f1be84172
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-10 12:17:59 +0530

    Time: 4 ms (44.86%), Space: 8.8 MB (19.03%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,16 +1,49 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        int freq[2001] = {0}, freqOfFreq[1001] = {0};
+        
+        int n = arr.size();
+  sort(arr.begin(), arr.end());
 
-        for (int& n : arr) freq[n + 1000]++;
+  // Create an auxiliary array to store the number of occurrences of each element.
+  vector<int> ans(n,0);
+  int p=0;
 
-        for (int i = 0; i < 2001; i++) {
-            if (freq[i] != 0 && freqOfFreq[freq[i]]++ > 0)
-                return false;
-        }
+  // Iterate over the input array and count the number of occurrences of each element.
+  for (int i = 0; i < n; i++) {
+    int c = 1;
+    int x=i;
 
-        return true;
+    // Compare the current element to the next element in the array.
+    // If the two elements are equal, increment the count for the current element and skip the next element.
+    for (int j =x+1; j < n; j++) {
+      if (arr[x] == arr[j]) {
+        c++;
+        i=j;
+      }
+    }
+
+  // Store the current count in the auxiliary array `ans`.
+    ans[p] = c;
+    p++;
+  }
+
+  // Iterate over the auxiliary array `ans` to check for duplicate counts.
+  bool uC = true;
+  for (int i = 0; i < p; i++) {
+    for (int j = i+1; j < p+1; j++) {
+      if (ans[i] == ans[j]) {
+        uC = false;
+        break; // Break out of the inner loop once a duplicate count is found
+      }
+    }
+    if (!uC) {
+      break; // Break out of the outer loop once a duplicate count is found
+    }
+  }
+
+  // Return `true` if there are no duplicate counts, otherwise return `false`.
+  return uC;
         
     }
 };
