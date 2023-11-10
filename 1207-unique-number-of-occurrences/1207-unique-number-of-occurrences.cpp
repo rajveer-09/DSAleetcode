2508c8111546ee5db7ac3d7e1ebf56a4c367c9a5
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-10 12:13:06 +0530

    Time: 4 ms (44.86%), Space: 8.5 MB (79.46%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,33 +1,15 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        int cnt = 1;
-        vector<int> ans;
-        int j = 1;
-        sort(arr.begin(), arr.end());
-        for(int i = 0; i < arr.size()-1; i++){
-            if(arr[i] == arr[j]){
-                j++;
-                cnt++;
-                continue;
-            }
-            // ans.push_back(cnt);
-            else if(arr[i] != arr[j]){
-                ans.push_back(cnt);
-                j += 1;
-                cnt = 1;
-                continue;
-            }
-            ans.push_back(cnt);
-            cnt = 1;
-        }
-        ans.push_back(cnt);
-        sort(ans.begin(), ans.end());
-        for(int i = 0; i < ans.size()-1; i++){
-            if(ans[i] == ans[i+1]){
+        int freq[2001] = {0}, freqOfFreq[1001] = {0};
+
+        for (int& n : arr) freq[n + 1000]++;
+
+        for (int i = 0; i < 2001; i++) {
+            if (freq[i] != 0 && freqOfFreq[freq[i]]++ > 0)
                 return false;
-            }
         }
+
         return true;
         
     }
