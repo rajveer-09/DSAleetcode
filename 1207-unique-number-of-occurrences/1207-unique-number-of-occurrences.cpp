5a49631b55e414ff942f1d75f297a16b3adc739f
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-11 13:16:55 +0530

    Time: 5 ms (36.71%), Space: 8.5 MB (64.85%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -2,30 +2,34 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         
-                // Sort the array to group equal elements together
+         int cnt = 1;
+        vector<int> ans;
+        int j = 1;
         sort(arr.begin(), arr.end());
-
-        int count = 1;
-        int n = arr.size();
-
-        unordered_set<int> occurrencesSet;
-
-        // Check for uniqueness of occurrences
-        for (int i = 1; i < n; ++i) {
-            if (arr[i] == arr[i - 1]) {
-                count++;
-            } else {
-                if (!occurrencesSet.insert(count).second) {
-                    // If count is not unique, return false
-                    return false;
-                }
-                count = 1; // Reset count for the next unique element
+        for(int i = 0; i < arr.size()-1; i++){
+            if(arr[i] == arr[j]){
+                j++;
+                cnt++;
+                continue;
+            }
+            // ans.push_back(cnt);
+            else if(arr[i] != arr[j]){
+                ans.push_back(cnt);
+                j += 1;
+                cnt = 1;
+                continue;
+            }
+            ans.push_back(cnt);
+            cnt = 1;
+        }
+        ans.push_back(cnt);
+        sort(ans.begin(), ans.end());
+        for(int i = 0; i < ans.size()-1; i++){
+            if(ans[i] == ans[i+1]){
+                return false;
             }
         }
-
-        // Check the last count
-        return occurrencesSet.insert(count).second;
-
+        return true;
         
     }
 };
