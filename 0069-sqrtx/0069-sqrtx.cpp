2651f34e3d6a410df4c6f47e17da2cb5c9591876
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-12 01:45:42 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (33.63%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
     int mySqrt(int x) {
-      //int s=0,e=x,ans=0;
+      int s=0,e=x,ans=0;
         
         
-        int n=x;
-       long int i=0;
-        //as ii should be an int if i is an int but integeroverflow can occur on i*i
         
-        for(;i*i<n;i++){
-           
-        }
+        while(s<=e){
+            int m=s+(e-s)/2;
+            if(m==0)
+                return x;
             
-         if(i*i>n)
-                return i-1;
-       return i;
+            if(m==x/m)//but here m=0 cannot be handled
+                return m;
+            
+            else if(m<x/m){
+                ans=m;
+                s=m+1;
+            }
+            
+            else{
+                e=m-1;
+            }
+            
+        }
+        
+        return ans;
+        
     }
         
 };
