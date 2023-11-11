1319fed95d554db9e0af589272d8da8999fe5e72
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-11 13:07:14 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (64.85%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -2,48 +2,30 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         
+                // Sort the array to group equal elements together
+        sort(arr.begin(), arr.end());
+
+        int count = 1;
         int n = arr.size();
-  sort(arr.begin(), arr.end());
 
-  // Create an auxiliary array to store the number of occurrences of each element.
-  vector<int> ans(n,0);
-  int p=0;
+        unordered_set<int> occurrencesSet;
 
-  // Iterate over the input array and count the number of occurrences of each element.
-  for (int i = 0; i < n; i++) {
-    int c = 1;
-    int x=i;
+        // Check for uniqueness of occurrences
+        for (int i = 1; i < n; ++i) {
+            if (arr[i] == arr[i - 1]) {
+                count++;
+            } else {
+                if (!occurrencesSet.insert(count).second) {
+                    // If count is not unique, return false
+                    return false;
+                }
+                count = 1; // Reset count for the next unique element
+            }
+        }
 
-    // Compare the current element to the next element in the array.
-    // If the two elements are equal, increment the count for the current element and skip the next element.
-    for (int j =x+1; j < n; j++) {
-      if (arr[x] == arr[j]) {
-        c++;
-        i=j;
-      }
-    }
+        // Check the last count
+        return occurrencesSet.insert(count).second;
 
-  // Store the current count in the auxiliary array `ans`.
-    ans[p] = c;
-    p++;
-  }
-
-  // Iterate over the auxiliary array `ans` to check for duplicate counts.
-  bool uC = true;
-  for (int i = 0; i < p; i++) {
-    for (int j = i+1; j < p+1; j++) {
-      if (ans[i] == ans[j]) {
-        uC = false;
-        break; // Break out of the inner loop once a duplicate count is found
-      }
-    }
-    if (!uC) {
-      break; // Break out of the outer loop once a duplicate count is found
-    }
-  }
-
-  // Return `true` if there are no duplicate counts, otherwise return `false`.
-  return uC;
         
     }
 };
