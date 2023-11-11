004aa5d9350b3028179808bdc453d1684cd1fb85
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-12 01:32:50 +0530

    Time: 9 ms (17.32%), Space: 6.2 MB (69.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -6,6 +6,7 @@ public:
         
         int n=x;
        long int i=0;
+        //as ii should be an int if i is an int but integeroverflow can occur on i*i
         
         for(;i*i<n;i++){
            
