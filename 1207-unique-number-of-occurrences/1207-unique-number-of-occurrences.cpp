9b2648c873ac075d7b29d028d432c22afb2d5b5e
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-11 14:24:37 +0530

    Update 1207-unique-number-of-occurrences.cpp

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,34 +1,52 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        int cnt = 1;
-        vector<int> ans;
-        int j = 1;
-        sort(arr.begin(), arr.end());
-        for(int i = 0; i < arr.size()-1; i++){
-            if(arr[i] == arr[j]){
-                j++;
-                cnt++;
-                continue;
-            }
-            // ans.push_back(cnt);
-            else if(arr[i] != arr[j]){
-                ans.push_back(cnt);
-                j += 1;
-                cnt = 1;
-                continue;
-            }
-            ans.push_back(cnt);
-            cnt = 1;
-        }
-        ans.push_back(cnt);
-        sort(ans.begin(), ans.end());
-        for(int i = 0; i < ans.size()-1; i++){
-            if(ans[i] == ans[i+1]){
-                return false;
+
+
+         vector<int> result;
+
+        for (int num : nums) {
+            int index = abs(num) - 1;
+
+            if (nums[index] < 0) {
+                // If the number at index is negative, it means we have seen it before
+                result.push_back(index + 1);
+            } else {
+                // Mark the number at index as seen by negating it
+                nums[index] = -nums[index];
             }
         }
-        return true;
-        
+
+        return result;
     }
+        // int cnt = 1;
+        // vector<int> ans;
+        // int j = 1;
+        // sort(arr.begin(), arr.end());
+        // for(int i = 0; i < arr.size()-1; i++){
+        //     if(arr[i] == arr[j]){
+        //         j++;
+        //         cnt++;
+        //         continue;
+        //     }
+        //     // ans.push_back(cnt);
+        //     else if(arr[i] != arr[j]){
+        //         ans.push_back(cnt);
+        //         j += 1;
+        //         cnt = 1;
+        //         continue;
+        //     }
+        //     ans.push_back(cnt);
+        //     cnt = 1;
+        // }
+        // ans.push_back(cnt);
+        // sort(ans.begin(), ans.end());
+        // for(int i = 0; i < ans.size()-1; i++){
+        //     if(ans[i] == ans[i+1]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        
+        //}
 };
