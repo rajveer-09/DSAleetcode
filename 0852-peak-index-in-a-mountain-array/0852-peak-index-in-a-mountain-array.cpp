7bc9ba6d0a6273d90b67e92a6385bffd5046c825
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-11 16:05:09 +0530

    Time: 88 ms (47.86%), Space: 59.9 MB (77.54%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-   int find_pivot(vector<int> v) {
-	int s = 0, e = v.size() - 1;
-	int mid=(s + e) / 2;
-	while (s < e)
-	{
-		if(v[mid] < v[mid+1])
-     			s=mid+1;
-    		else
-      			e = mid;
-    
-		mid = (s + e) / 2;
-	}
-	return s;
-    }
     int peakIndexInMountainArray(vector<int>& arr) {
-        return find_pivot(arr);
-    }
+        int left = 0;
+        int right = arr.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                // We are in the ascending part of the mountain
+                left = mid + 1;
+            } else {
+                // We are in the descending part of the mountain or at the peak
+                right = mid;
+            }
+        }
+
+        // The loop breaks when left == right, which is the peak
+        return left;
         
+    }
 };
