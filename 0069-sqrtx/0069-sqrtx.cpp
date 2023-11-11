5b96552679a2c3ee62d32bbe7bfb97b3fbab6aed
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2023-11-12 00:48:33 +0530

    Time: 6 ms (18.11%), Space: 6.3 MB (33.06%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        
+        int n=x;
+       long long int i=0;
+        
+        for(;i*i<n;i++){
+           
+        }
+            
+         if(i*i>n)
+                return i-1;
+       return i;
+    }
+};
