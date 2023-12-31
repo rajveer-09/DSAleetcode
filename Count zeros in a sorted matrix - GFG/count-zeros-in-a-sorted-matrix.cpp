3224bb9d7950e9771a35ad5c8d73cb58e082cb73
rajveer-09 <143857709+rajveer-09@users.noreply.github.com>
2024-01-01 01:36:23 +0530

    Added solution - LeetHub

--- Count zeros in a sorted matrix - GFG/count-zeros-in-a-sorted-matrix.cpp
+++ Count zeros in a sorted matrix - GFG/count-zeros-in-a-sorted-matrix.cpp
@@ -11,13 +11,23 @@ public:
 	int countZeros(vector<vector<int>>A)
 	{
 		//code here
-		int len = A.size();
-		int len2 = len * len;
-		int count = 0;
-		for(int i=0;i<len2;i++){
-		    if(A[i/len][i%len]==0)count ++;
-		}
-		return count;
+	int N = A.size();
+    int count = 0;
+    
+    // Start from the top-right corner
+    int i = 0, j = N - 1;
+
+    while (i < N && j >= 0) {
+        // If the current element is zero, move to the next row
+        if (A[i][j] == 0) {
+            count += j + 1;  // Count the zeros to the left of the current position
+            i++;            // Move to the next row
+        } else {
+            j--;            // Move to the left in the same row
+        }
+    }
+
+    return count;
 	}
 };
 
